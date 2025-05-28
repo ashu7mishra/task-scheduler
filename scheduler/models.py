759f038694e64ae9a1a2dd96131baeb27b79ec76
ashu7mishra <ashu7mishra@gmail.com>
2025-05-28 10:52:28 +0530

    added models

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import uuid
+from enum import Enum
 
-# Create your models here.
+
+class TaskStatus(models.TextChoices):
+    PENDING = "PENDING", "Pending"
+    IN_PROGRESS = "IN_PROGRESS", "In Progress"
+    COMPLETED = "COMPLETED", "Completed"
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(
+        max_length = 20,
+        choices = TaskStatus.choices,
+        default = TaskStatus.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name} [{self.status}]"
+    
+    
+class TaskDependency(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    task = models.ForeignKey(Task, related_name='dependent_task', on_delete=models.CASCADE)
+    depends_on = models.ForeignKey(Task, related_name='prerequisite_tasks', on_delete=models.CASCADE)
+    
+    class meta:
+        unique_together = ('task', 'depends_on')
+        
+    def __str__(self):
+        return f"{self.task.name} depends on {self.depends_on.name}"
+        
