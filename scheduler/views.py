a081a8658528911e86227debc158624640761093
ashu7mishra <ashu7mishra@gmail.com>
2025-06-06 08:01:43 +0530

    views complete

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, views, response
 from .models import Task, TaskDependency
 from .serializer import TaskSerializer, TaskDependencySerializer
 
@@ -16,7 +16,6 @@ class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     
     
 # task dependency views
-
 class TaskDependencyListCreateAPIView(generics.ListCreateAPIView):
     queryset = TaskDependency.objects.all()
     serializer_class = TaskDependency
@@ -24,3 +23,30 @@ class TaskDependencyListCreateAPIView(generics.ListCreateAPIView):
 class TaskDependencyRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TaskDependency.objects.all()
     serializer_class = TaskDependency
+    
+    
+# Graph + scheduling view
+class TaskExecutionOrderView(views.APIView):
+    def get(self, request):
+        graph, all_nodes = GraphBuilder().build_graph()
+        scheduler = SchedulerService(graph, all_nodes)
+        
+        if scheduler.has_cycle():
+            return response.Response(
+                {'error': 'Cycle Detected. Cannot generate execution order.'}
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        order = scheduler.topological_sort()
+        ordered_task = Task.objects.filter(id__in=order)
+        serialized = TaskSerializer(ordered_task, many=True)
+        return response.Response(serialized.data)
+    
+    
+class TaskDetectionView(views.APIView):
+    def get(self, request):
+        graph, all_nodes = GraphBuilder().build_graph()
+        scheduler = SchedulerService(graph, all_nodes)
+        return response.Response({"has cycle:", scheduler.has_cycle()})
+        
+    
