98dc7247a30493886691e377b47ce9211f3afb56
ashu7mishra <ashu7mishra@gmail.com>
2025-06-06 06:03:43 +0530

    updated views

--- scheduler/serializer.py
+++ scheduler/serializer.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, TaskDependency
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
+        fields = '__all__'
+        
+class TaskDependencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskDependency
         fields = '__all__'
         
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from .models import Task, TaskDependency
+from .serializer import TaskSerializer, TaskDependencySerializer
 
-# Create your views here.
+
+# task views
+class TaskListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+
+class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+    
+# task dependency views
+
+class TaskDependencyListCreateAPIView(generics.ListCreateAPIView):
+    queryset = TaskDependency.objects.all()
+    serializer_class = TaskDependency
+    
+class TaskDependencyRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TaskDependency.objects.all()
+    serializer_class = TaskDependency
